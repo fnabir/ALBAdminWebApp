4860a816bf8f339069d30e40d591a8d3e7b9dcd9
Farhan Noor Abir <fnoor.abir@gmail.com>
2024-09-20 22:06:17 +0100

    Create login and logout function

---
+++ src/firebase/auth.tsx
@@ -0,0 +1,18 @@
+import '@/firebase/config'
+import {auth} from '@/firebase/config'
+import { onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+
+// Login the user
+export const login = (email: string, password: string) => {
+	return signInWithEmailAndPassword(auth, email, password);
+};
+
+// Logout the user
+export const logout = async () => {
+	try {
+		await signOut(auth)
+		console.log("Logged out successfully.")
+	} catch (error) {
+		console.error("Error logging out: ", error)
+	}
+};
