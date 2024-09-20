1672c89b0adc5253011354305c5372b5dbb3812c
Farhan Noor Abir <fnoor.abir@gmail.com>
2024-09-20 22:02:26 +0100

    Create TotalBalance Component

---
+++ src/components/TotalBalance.tsx
@@ -0,0 +1,23 @@
+import { formatCurrency } from "@/utils/functions";
+import { useRouter } from "next/navigation"
+
+export default function TotalBalance(props:TotalBalanceInterface) {
+    const router = useRouter();
+    const titleText = props.text ? props.text : "Total Balance";
+    const dateText = props.date == null || props.date == '' ? 'Last update date not found' : 'Last updated on ' + (props.date);
+
+    return (
+        <div className="rounded-lg shadow bg-slate-700 hidden md:block">
+            <div className="w-full mx-auto px-6 pt-2 md:flex md:items-center md:justify-between text-white">
+                <div>
+                    <div className="text-xl  sm:text-center md:text-start">{titleText}</div>
+                    <p id="updatedate" className="text-sm pb-2 sm:text-center md:text-start">{dateText}</p>
+                </div>
+                <text className="flex flex-wrap items-center mt-3 text-3xl font-medium sm:mt-0">
+                    {formatCurrency(props.value)}
+                </text>
+            </div>
+            
+        </div>
+    )
+}
