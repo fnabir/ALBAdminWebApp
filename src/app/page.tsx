86f96022b97ef9b9a0412e2e3edc869fc9f8eff8
Farhan Noor Abir <fnoor.abir@gmail.com>
2024-09-27 21:41:32 +0100

    Update Dashboard Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,19 @@
 "use client"
 
-import CardBalance from "@/components/card/CardBalance";
+import CardBalance from "@/components/card/CardTotalBalance";
 import { MdOutlineBuild , MdOutlineLocalOffer, MdErrorOutline } from "react-icons/md";
 import CardIcon from "@/components/card/CardIcon";
 import { useAuth } from "@/context/AuthContext";
 import Layout from "@/components/Layout";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import { get, ref } from "firebase/database";
-import { database } from "@/firebase/config";
 import Loading from "@/components/Loading";
+import { getObjectDataWithTotal } from "@/firebase/database";
 
 export default function Home() {
-  const [dataLoading, setDataLoading] = useState(true)
-  const [projectBalance, setProjectBalance] = useState<TotalBalanceInterface>({value: 0, date:""});
-  const [staffBalance, setStaffBalance] = useState<TotalBalanceInterface>({value: 0, date:""});
-  const [conveyanceBalance, setConveyanceBalance] = useState<TotalBalanceInterface>({value: 0, date:""});
   const router = useRouter();
   const { user, loading } = useAuth();
-
-  const projectBalanceRef = ref(database, 'balance/total/project');
-  const staffBalanceRef = ref(database, 'balance/total/staff');
-  const conveyanceBalanceRef = ref(database, 'balance/total/conveyance');
   
-  useEffect(() => {
-    if (dataLoading) {
-      get(projectBalanceRef).then((snapshot) => {
-        console.log("Project balance");
-        if (snapshot.exists()) {
-          setProjectBalance({value: snapshot.val().value, date: snapshot.val().date});
-        } else {
-          console.log('No data available');
-        }
-      }).catch((error) => {
-        console.log(error);
-      });
-
-      get(staffBalanceRef).then((snapshot) => {
-        console.log("Staff balance");
-        if (snapshot.exists()) {
-          setStaffBalance({value: snapshot.val().value, date: snapshot.val().date});
-        } else {
-          console.log('No data available');
-        }
-      }).catch((error) => {
-        console.log(error);
-      });
-
-      get(conveyanceBalanceRef).then((snapshot) => {
-        console.log("Conveyance balance");
-        if (snapshot.exists()) {
-          setConveyanceBalance({value: snapshot.val().value, date: snapshot.val().date});
-        } else {
-          console.log('No data available');
-        }
-      }).catch((error) => {
-        console.log(error);
-      });
-
-      setDataLoading(false)
-    }
-  });
+  const { data, dataLoading, error } = getObjectDataWithTotal('balance/total');
 
   while (loading) return <Loading/>
   if (!loading && !user) return router.push("login")
@@ -71,21 +24,17 @@ export default function Home() {
         headerTitle="Dashboard">
         <div className="h-full">
           <div className='flex flex-col md:flex-row justify-around space-x-0 md:space-x-2 space-y-2 md:space-y-0 mt-3'>
-            <CardBalance 
-              title="Project Balance" 
-              balance={projectBalance.value} 
-              date={projectBalance.date}
-              route='projects'/>
-            <CardBalance
-              title="Staff Balance" 
-              balance={staffBalance.value} 
-              date={staffBalance.date} 
-              route='staff'/>
-            <CardBalance 
-            title="Conveyance Balance" 
-            balance={conveyanceBalance.value} 
-            date={conveyanceBalance.date} 
-            route='conveyance'/>
+              { 
+                dataLoading ? (
+                  <CardBalance title={"Loading"} balance={0} date={"Loading"}/>
+                ) : error ? (
+                  <CardBalance title={"Error Occured"} balance={0} date={error}/>
+                ) : (
+                  data.sort((a:any, b:any) => b.value - a.value).map((item) => (
+                    <CardBalance title={item.key} balance={item.value} date={item.date} route={item.key}/>
+                  ))
+                )
+              }
           </div>
 
           <div className='flex flex-col md:flex-row justify-around space-x-0 md:space-x-2 space-y-2 md:space-y-0 mt-3'>
