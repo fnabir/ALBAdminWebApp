eed559d547d8b7467165c9765bbdf5685892b379
Farhan Noor Abir <fnoor.abir@gmail.com>
2024-09-27 21:40:56 +0100

    Update Staff Page

--- src/app/staff/page.tsx
+++ src/app/staff/page.tsx
@@ -5,41 +5,20 @@ import Loading from "@/components/Loading";
 import TotalBalance from "@/components/TotalBalance";
 import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
-import { database } from "@/firebase/config";
-import { child, get, ref } from "firebase/database";
-import { useEffect, useState } from "react";
+import CardIcon from "@/components/card/CardIcon";
+import { MdDownloading, MdError } from "react-icons/md";
+import CardBalance from "@/components/card/CardBalance";
+import { getDatabaseValue, getObjectDataWithTotal } from "@/firebase/database";
 
 export default function Staff() {
   const { user, loading } = useAuth();
   const router = useRouter();
 
-  const [dataLoading, setDataLoading] = useState(true)
-  const [staffBalance, setStaffBalance] = useState<TotalBalanceInterface>({value: 0, date:""});
-  const [conveyanceBalance, setConveyanceBalance] = useState<TotalBalanceInterface>({value: 0, date:""});
+  const { data, total, dataLoading, error } = getObjectDataWithTotal('balance/staff');
 
-  useEffect(() => {
-    if (dataLoading) {
-      get(child(ref(database), 'balance/total/staff')).then((snapshot) => {
-        if (snapshot.exists()) {
-          setStaffBalance({value: snapshot.val().value, date: snapshot.val().date});
-        } else {
-          setStaffBalance({value: 0, date: ""});
-        }
-      }).catch((error) => {
-        console.error(error);
-      });
-      get(child(ref(database), 'balance/total/conveyance')).then((snapshot) => {
-        if (snapshot.exists()) {
-          setConveyanceBalance({value: snapshot.val().value, date: snapshot.val().date});
-        } else {
-          setConveyanceBalance({value: 0, date: ""});
-        }
-      }).catch((error) => {
-        console.error(error);
-      });
-      setDataLoading(false)
-    }
-  });
+  const staffBalanceDate = getDatabaseValue("balance/total/staff/date").data;
+  const conveyanceBalance = getDatabaseValue("balance/total/conveyance/value").data;
+  const conveyanceBalanceDate = getDatabaseValue("balance/total/staff/date").data;
 
   while (loading) return <Loading/>
   if (!loading && !user) return router.push("login")
@@ -48,9 +27,24 @@ export default function Staff() {
       <Layout 
         pageTitle="Staff | Asian Lift Bangladesh"
         headerTitle="Staff">
-          <div className="flex flex-col-reverse h-full py-2 gap-y-2">
-            <TotalBalance text="Total Conveyance" value={conveyanceBalance.value} date={conveyanceBalance.date}/>
-            <TotalBalance value={staffBalance.value + conveyanceBalance.value} date={staffBalance.date}/>
+          <div className="flex flex-col h-full py-2 gap-y-2">
+            {
+              dataLoading ? (
+                <CardIcon title={"Loading"} subtitle={"If data doesn't load in 30 seconds, please refresh the page."}>
+                  <MdDownloading className='mx-1 w-6 h-6 content-center'/>
+                </CardIcon>
+              ) : error ? (
+                <CardIcon title={"Error"} subtitle={error? error : ""}>
+                  <MdError className='mx-1 w-6 h-6 content-center'/>
+                </CardIcon>
+              ) : (
+                data.map((item) => (
+                  <CardBalance name={item.name} value={item.value} date={item.date} status={item.status}/>
+                ))
+              )
+            }
+            <TotalBalance text="Total Conveyance" value={conveyanceBalance} date={conveyanceBalanceDate}/>
+            <TotalBalance value={total + conveyanceBalance} date={staffBalanceDate}/>
           </div>
       </Layout>
     );
