d5d5bbc85414a64a44ed4874d4f4c8aa0dcd57bc
Farhan Noor Abir <fnoor.abir@gmail.com>
2024-09-29 18:40:09 +0100

    Change server file extension to ts

--- server.ts
+++ server.ts
@@ -12,4 +12,10 @@ app.prepare().then(() => {
     const parsedUrl = parse(req.url!, true)
     handle(req, res, parsedUrl)
   }).listen(port)
+ 
+  console.log(
+    `> Server listening at http://localhost:${port} as ${
+      dev ? 'development' : process.env.NODE_ENV
+    }`
+  )
 })
