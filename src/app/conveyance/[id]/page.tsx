961771c6e61723758eb500190e8c3ba799b825e6
Farhan Noor Abir <fnoor.abir@gmail.com>
2024-09-30 13:08:22 +0100

    Create Conveyance Transaction Page

---
+++ src/app/conveyance/[id]/page.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import Layout from "@/components/Layout";
+import Loading from "@/components/Loading";
+import TotalBalance from "@/components/TotalBalance";
+import { useAuth } from "@/context/AuthContext";
+import { useRouter, usePathname } from "next/navigation";
+import CardIcon from "@/components/card/CardIcon";
+import { MdDownloading, MdError } from "react-icons/md";
+import { getDatabaseValue, getObjectDataWithTotal } from "@/firebase/database";
+import CardTransaction from "@/components/card/CardTransaction";
+
+export default function ProjectTransaction() {
+  const { user, loading } = useAuth();
+  const router = useRouter();
+  const path = usePathname();
+
+  while (loading) return <Loading/>
+  if (!loading && !user) return router.push("login");
+  else{
+    const staffID: string = decodeURIComponent(path.substring(path.lastIndexOf("/") + 1));
+    const staffName = getDatabaseValue("balance/conveyance/" + staffID + "/name").data;
+    const { dataExist, data, total, dataLoading, error } = getObjectDataWithTotal('transaction/conveyance/' + staffID);
+    const totalBalanceDate = getDatabaseValue("balance/conveyance/" + staffID + "/date").data;
+    if (error) return router.push("/staff");
+    else {
+      return (
+        <Layout 
+          pageTitle={staffName + " | Asian Lift Bangladesh"}
+          headerTitle={staffName}>
+            <div className="flex flex-col h-full py-2 gap-y-2">
+              {
+                dataLoading ? (
+                  <CardIcon title={"Loading"} subtitle={"If data doesn't load in 30 seconds, please refresh the page."}>
+                    <MdDownloading className='mx-1 w-6 h-6 content-center'/>
+                  </CardIcon>
+                ) : error ? (
+                  <CardIcon title={"Error"} subtitle={error? error : ""}>
+                    <MdError className='mx-1 w-6 h-6 content-center'/>
+                  </CardIcon>
+                ) : !dataExist ? (
+                  <CardIcon title={"No Record Found!"} subtitle={""}>
+                    <MdError className='mx-1 w-6 h-6 content-center'/>
+                  </CardIcon>
+                ) : (
+                  data.map((item) =>
+                    (
+                      <CardTransaction title={item.title} amount={item.amount} date={item.date} details={item.details} id={item.key}/>
+                    )
+                  )
+                )
+              }
+              <TotalBalance value={total} date={totalBalanceDate}/>
+            </div>
+          
+        </Layout>
+      );
+    }
+  }
+};
