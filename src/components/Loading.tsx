ef79f7ea316fe6c25922f9e34f1316381e8d60d6
Farhan Noor Abir <fnoor.abir@gmail.com>
2024-10-18 08:36:15 +0100

    Update Loading screen to auto refresh after 10 seconds

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
+
 export default function Loading() {
+	const [showTime, setShowTime] = useState(10000);
+	const [loaded, setloaded] = useState(true);
+
+	useEffect(() => { 
+		const timeoutId = setTimeout(() => {
+			window.location.reload();
+		}, 10000);
+	
+		return () => clearTimeout(timeoutId);
+	  }, []);
+
+	  useEffect(() => {
+		const showTimer = setTimeout(() => {
+			setShowTime(showTime-1000);
+		}, 1000);
+	  }, [showTime]);
+
 	return (
 		<div className='w-full h-screen flex items-center flex-col justify-center'>
-			<h2 className='text-2xl font-bold'>Loading, please wait...</h2>
+			<span className="relative flex h-10 w-10 justify-center items-center m-8">
+				<span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+				<span className="relative inline-flex rounded-full h-5 w-5 bg-sky-500"></span>
+			</span>
+			<h2 className='text-2xl font-bold'>{showTime < 0 ? "Please refresh" : "Loading, please wait..."}</h2>
+			<p>{showTime > 0 ? `If the page doesn't load in ${showTime/1000} seconds, it will reload automatically` : ""}</p>
 		</div>
 	);
 };
