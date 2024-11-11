532af7a891632661c14d62da8f1dbac4b809c974
Farhan Noor Abir <fnoor.abir@gmail.com>
2024-11-11 08:30:10 +0000

    Remove redundant function and optimized functions handling promise

--- src/firebase/database.tsx
+++ src/firebase/database.tsx
@@ -1,4 +1,4 @@
-import { ref, child, get, remove, set, update } from "firebase/database";
+import { ref, child, get } from "firebase/database";
 import { database } from "@/firebase/config";
 import { useEffect, useState } from "react";
 import { auth } from '@/firebase/config'
@@ -11,19 +11,15 @@ export default function GetDataExist(databaseReference: string): any {
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    const fetchData = async () => {
-      get(child(ref(database), databaseReference)).then((snapshot) => {
-        if (snapshot.exists()) {
-          setDataExist(true)
-        }
-      }).catch((error) => {
-        console.error(error.message);
-        setError(error.message)
-      });
-      setDataLoading(false);
-    }
-
-    fetchData();
+      get(child(ref(database), databaseReference))
+        .then((snapshot) => {
+          setDataExist(snapshot.exists())
+        }).catch((error) => {
+          console.error(error.message);
+          setError(error.message)
+        }).finally(() => {
+          setDataLoading(false);
+        })
   }, [databaseReference]);
 
   return { dataExist, dataLoading, error };
@@ -36,19 +32,17 @@ export function GetDataCount(databaseReference: string): any {
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    const fetchData = async () => {
-      get(child(ref(database), databaseReference)).then((snapshot) => {
-        if (snapshot.exists()) {
-          setDataCount(snapshot.size)
-        } else setDataCount(0)
-      }).catch((error) => {
-        console.error(error.message);
-        setError(error.message)
-      });
-      setDataLoading(false);
-    }
-    
-    fetchData();
+    get(child(ref(database), databaseReference))
+        .then((snapshot) => {
+          if (snapshot.exists()) {
+            setDataCount(snapshot.size)
+          } else setDataCount(0)
+        }).catch((error) => {
+          console.error(error.message);
+          setError(error.message)
+        }).finally(() => {
+        setDataLoading(false);
+        })
   }, [databaseReference]);
 
   return { dataCount, dataLoading, error };
@@ -62,24 +56,20 @@ export function GetDatabaseValue(databaseReference: string): any {
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    const fetchData = async () => {
-      get(child(ref(database), databaseReference)).then((snapshot) => {
-        if (snapshot.exists()) {
-          setDataExist(true)
-          setData(snapshot.val());
-        } else {
-          console.log("No data available");
-          setData([])
-        }
-      }).catch((error) => {
-        console.error(error.message);
-        setError(error.message)
-      });
-  
-      setDataLoading(false);
-    }
-    
-    fetchData();
+    get(child(ref(database), databaseReference))
+        .then((snapshot) => {
+          if (snapshot.exists()) {
+            setDataExist(true);
+            setData(snapshot.val());
+          } else {
+            setDataExist(false);
+          }
+        }).catch((error) => {
+          console.error(error.message);
+          setError(error.message)
+        }).finally(() => {
+          setDataLoading(false);
+        })
   }, [databaseReference]);
 
   return { dataExist, data, dataLoading, error };
@@ -93,12 +83,11 @@ export const GetObjectData = (databaseReference: string) => {
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    const fetchData = async () => {
       get(child(ref(database), databaseReference)).then((snapshot) => {
         if (snapshot.exists()) {
-          setDataExist(true)
-          const snapshotDatas = snapshot.val();
-          const itemsWithKeys = Object.entries(snapshotDatas).map(([key, value]) => {
+          setDataExist(true);
+          const snapshotValue = snapshot.val();
+          const itemsWithKeys = Object.entries(snapshotValue).map(([key, value]) => {
             if (typeof value === 'object' && value !== null) {
               return { key, ...value, childCount: Object.keys(value).length };
             } else {
@@ -107,18 +96,14 @@ export const GetObjectData = (databaseReference: string) => {
           });
           setData(itemsWithKeys);
         } else {
-          console.log("No data available");
-          setData([])
+          setDataExist(false)
         }
       }).catch((error) => {
         console.error(error.message);
         setError(error.message)
-      });
-  
-      setDataLoading(false);
-    }
-    
-    fetchData();
+      }).finally(() => {
+        setDataLoading(false);
+      })
   }, [databaseReference]);
 
   return { dataExist, data, dataLoading, error };
@@ -133,149 +118,80 @@ export const GetObjectDataWithTotal = (databaseReference: string) => {
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    const fetchData = async () => {
-      get(child(ref(database), databaseReference)).then((snapshot) => {
-        if (snapshot.exists()) {
-          setDataExist(true);
-          const snapshotDatas = snapshot.val();
-          const itemsWithKeys = Object.entries(snapshotDatas).map(([key, value]) => {
-            if (typeof value === 'object' && value !== null) {
-              return { key, ...value, childCount: Object.keys(value).length };
-            } else {
-              return null;
-            }
-          });
-          setData(itemsWithKeys);
-          const totalValues = Object.values(snapshotDatas).reduce((sum, snapshotData) => {
-            if (typeof snapshotData === 'object' && snapshotData != null && typeof sum === 'number') {
-              if ('value' in snapshotData && typeof snapshotData.value === 'number') return sum + snapshotData.value;
-              else if ('amount' in snapshotData && typeof snapshotData.amount === 'number') return sum + snapshotData.amount;
-            }
-            return sum;
-          }, 0);
-          setTotal(Number(totalValues));
-        } else {
-          console.log("No data available");
-          setData([]);
-          setTotal(0);
-        }
-      }).catch((error) => {
-        console.error(error.message);
-        setError(error.message)
-      });
-  
-      setDataLoading(false);
-    }
-    
-    fetchData();
-  }, [databaseReference]);
-
-  return { dataExist, data, total, dataLoading, error };
-};
-
-
-export function GetUserRole(userUid: string): any {
-  const [role, setRole] = useState("");
-
-  useEffect(() => {
-    const fetchData = async () => {
-      get(child(ref(database), "info/user/" + userUid)).then((snapshot) => {
-        if (snapshot.exists()) {
-          setRole(snapshot.val().role);
-        } else {
-          console.log("No data available");
-          setRole("")
-        }
-      }).catch((error) => {
-        console.error(error.message);
-      });
-    }
-    
-    fetchData();
-  }, [userUid]);
-
-  return role;
-}
-
-
-export const SetData = (databaseReference: string, data: object) => {
-  const [total, setTotal] = useState(0);
-  const [dataUploading, setDataUploading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const writeData = async () => {
-      setDataUploading(true);
-
-      set(ref(database, databaseReference), data)
-      .then(() => {
-        console.log("Data saved successfully!");
-      })
-      .catch((error) => {
-        console.error(error.message);
-        setError(error.message);
-      })
-      .finally(() => {
-        setDataUploading(false);
-      })
-    };
-
-    writeData();
-  }, [databaseReference, data]);
-};
-
-
-export const UpdateData = (databaseReference: string, data: object) => {
-  const [total, setTotal] = useState(0);
-  const [dataUploading, setDataUploading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const updateData = async () => {
-      setDataUploading(true);
-
-      update(ref(database, databaseReference), data)
-      .then(() => {
-        console.log("Data saved successfully!");
-      })
-      .catch((error) => {
-        console.error(error.message);
-        setError(error.message);
-      })
-      .finally(() => {
-        setDataUploading(false);
-      })
-    };
-
-    updateData();
-  }, [databaseReference, data]);
-};
-
-
-export const DeleteData = (databaseReference: string) => {
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const removeData = async () => {
-      setIsDeleting(true);
-
-      try {
-        await remove(child(ref(database), databaseReference));
-        console.log('Data deleted successfully!');
-      } catch (error) {
-        console.error('Error deleting data:', error);
-        setError(String(error));
-      } finally {
-        setIsDeleting(false);
+    get(child(ref(database), databaseReference)).then((snapshot) => {
+      if (snapshot.exists()) {
+        setDataExist(true);
+        const snapshotValue = snapshot.val();
+        const itemsWithKeys = Object.entries(snapshotValue).map(([key, value]) => {
+          if (typeof value === 'object' && value !== null) {
+            return { key, ...value, childCount: Object.keys(value).length };
+          } else {
+            return null;
+          }
+        });
+        setData(itemsWithKeys);
+        const totalValues = Object.values(snapshotValue).reduce((sum, snapshotData) => {
+          if (typeof snapshotData === 'object' && snapshotData != null && typeof sum === 'number') {
+            if ('value' in snapshotData && typeof snapshotData.value === 'number') return sum + snapshotData.value;
+            else if ('amount' in snapshotData && typeof snapshotData.amount === 'number') return sum + snapshotData.amount;
+          }
+          return sum;
+        }, 0);
+        setTotal(Number(totalValues));
+      } else {
+        setDataExist(false);
       }
-    };
-
-    removeData();
+    }).catch((error) => {
+      setError(error.message);
+    }).finally(() => {
+      setDataLoading(false);
+    })
   }, [databaseReference]);
-  return { isDeleting, error };
-}
 
+  return { error, dataExist, data, total, dataLoading };
+};
+
+
+export const useGetObjectDataWithTotal = (databaseReference: string) => {
+  const [dataExist, setDataExist] = useState(false);
+  const [data, setData] = useState<any[]>([]);
+  const [total, setTotal] = useState(0);
+  const [dataLoading, setDataLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    get(child(ref(database), databaseReference)).then((snapshot) => {
+      if (snapshot.exists()) {
+        setDataExist(true);
+        const snapshotValue = snapshot.val();
+        const itemsWithKeys = Object.entries(snapshotValue).map(([key, value]) => {
+          if (typeof value === 'object' && value !== null) {
+            return { key, ...value, childCount: Object.keys(value).length };
+          } else {
+            return null;
+          }
+        });
+        setData(itemsWithKeys);
+        const totalValues = Object.values(snapshotValue).reduce((sum, snapshotData) => {
+          if (typeof snapshotData === 'object' && snapshotData != null && typeof sum === 'number') {
+            if ('value' in snapshotData && typeof snapshotData.value === 'number') return sum + snapshotData.value;
+            else if ('amount' in snapshotData && typeof snapshotData.amount === 'number') return sum + snapshotData.amount;
+          }
+          return sum;
+        }, 0);
+        setTotal(Number(totalValues));
+      } else {
+        setDataExist(false);
+      }
+    }).catch((error) => {
+      setError(error.message);
+    }).finally(() => {
+      setDataLoading(false);
+    })
+  }, [databaseReference]);
+
+  return { error, dataExist, data, total, dataLoading };
+};
 
 export const UpdateUserDisplayName = (newDisplayName: string) => {
     if (auth.currentUser) {
