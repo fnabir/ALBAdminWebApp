a9131653710cd20b957d45811a9a9826852e29e3
Farhan Noor Abir <fnoor.abir@gmail.com>
2024-11-29 09:27:33 +0000

    Create utils/arrays

---
+++ src/utils/arrays.tsx
@@ -0,0 +1,17 @@
+export const OfferProductOptions=[
+	{value: 'Passenger Lift'},
+	{value: 'Cargo Lift'},
+	{value: 'Hospital Lift'},
+	{value: 'Capsule Lift'},
+	{value: 'Escalator'},
+	{value: 'Dumbwaiter'},
+	{value: 'Generator'},
+	{value: 'Other'}
+]
+
+export const OfferWorkOptions = [
+	{value: 'Full Project'},
+	{value: 'Servicing'},
+	{value: 'Installation'},
+	{value: 'Repair'}
+];
