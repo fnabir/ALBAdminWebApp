a0dbe04a11082b1304a5f797e1e3cede6a554bf7
Farhan Noor Abir <fnoor.abir@gmail.com>
2024-12-05 21:06:30 +0000

    Create CustomButtonGroup component

---
+++ src/components/generic/CustomButtonGroup.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+type ButtonGroupProps = {
+	value?: string;
+	options: { label?: string; value: string }[];
+	onChange: (value: string) => void;
+};
+
+const ButtonGroup: React.FC<ButtonGroupProps> = ({ value, options, onChange }) => {
+	const [selected, setSelected] = useState<string>(value ? value : options[0]?.value || "");
+
+	const handleClick = (value: string) => {
+		setSelected(value);
+		onChange(value);
+	};
+
+	return (
+		<div className={`flex gap-x-2`}>
+			{options.map((option) => (
+				<button
+					key={option.value}
+					onClick={() => handleClick(option.value)}
+					className={`font-semibold py-1 px-4 rounded-full border border-blue-400 hover:bg-blue-800 ${selected === option.value ? "bg-blue-600 text-white" : "bg-transparent text-blue-400 hover:text-white hover:border-transparent"}`}
+				>
+					{option.label ? option.label : option.value}
+				</button>
+			))}
+		</div>
+	);
+};
+
+export default ButtonGroup;
