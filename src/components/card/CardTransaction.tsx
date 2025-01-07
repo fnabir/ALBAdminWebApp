b101c66c52d1a579a765acf04860606c980cf34b
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-01-07 09:58:16 +0000

    Simplify Staff and Conveyance Transaction component

--- src/components/card/CardTransaction.tsx
+++ src/components/card/CardTransaction.tsx
@@ -1,4 +1,4 @@
-import { errorMessage, formatCurrency, successMessage } from "@/utils/functions";
+import {errorMessage, formatCurrency, successMessage} from "@/utils/functions";
 import { MdDelete, MdEditNote } from "react-icons/md";
 import { Modal } from "flowbite-react";
 import { useState} from "react";
@@ -8,25 +8,22 @@ import { Button } from "flowbite-react";
 import {remove, update} from "firebase/database";
 import {format, parse} from "date-fns";
 import {formatInTimeZone} from "date-fns-tz";
-import {useList} from "react-firebase-hooks/database";
-import {GetDatabaseReference, GetTotalValue} from "@/firebase/database";
-import UniqueChildren from "@/components/UniqueChildrenWrapper";
+import {GetDatabaseReference} from "@/firebase/database";
+import CustomDateTimeInput from "@/components/generic/CustomDateTimeInput";
 
 
 export default function CardTransaction(props:TransactionInterface) {
     const [editModal, setEditModal] = useState(false);
     const [deleteModal, setDeleteModal] = useState(false);
 
-    const [inputTitle, setInputTitle] = useState(props.title);
-    const [inputDetails, setInputDetails] = useState(props.details);
-    const [inputAmount, setInputAmount] = useState(Math.abs(props.amount));
-    const inputAmountType = props.amount < 0 ? "-" : "+";
-    const [inputDate, setInputDate] = useState(format(parse(props.date, "yy.MM.dd", new Date()), 'yyyy-MM-dd'));
-    const detailsText = props.details;
     const databaseRef = `transaction/${props.type}/${props.uid}/${props.transactionId}`;
-    const [data] =  useList(GetDatabaseReference(`${databaseRef}/data`));
-    const total = GetTotalValue(data, "amount");
-    const bg = props.amount <= 0 || props.amount == total? 'bg-green-900' : props.amount > 0 ? props.amount < total ? 'bg-amber-900' : 'bg-red-900' : 'bg-slate-700';
+    const [transactionData, setTransactionData] = useState({
+        type: props.amount < 0 ? '-' : '+',
+        title: props.title,
+        details: props.details,
+        amount: Math.abs(props.amount),
+        date: format(parse(props.date, "yy.MM.dd", new Date()), 'yyyy-MM-dd'),
+    })
 
     const updateDate = async() => {
         const today = new Date();
@@ -36,9 +33,9 @@ export default function CardTransaction(props:TransactionInterface) {
         update(GetDatabaseReference(`balance/total/${props.type}`), {
             date: formattedDate,
         }).catch((error) => {
-                console.error(error.message);
-                errorMessage(error.message);
-            })
+            console.error(error.message);
+            errorMessage(error.message);
+        })
 
         update(GetDatabaseReference(`balance/${props.type}/${props.uid}`), {
             date: formattedDate,
@@ -50,9 +47,9 @@ export default function CardTransaction(props:TransactionInterface) {
 
     const handleEdit = async () => {
         const updatedData = {
-            title: inputTitle,
-            details: inputDetails,
-            amount: inputAmountType == "+" ? Math.abs(inputAmount) : Math.abs(inputAmount) * (-1),
+            title: transactionData.title,
+            details: transactionData.details,
+            amount: transactionData.type == "+" ? Math.abs(transactionData.amount) : Math.abs(transactionData.amount) * (-1),
         }
         if (props.title == "") {
             errorMessage("Please input title")
@@ -62,8 +59,8 @@ export default function CardTransaction(props:TransactionInterface) {
         } else {
             update(GetDatabaseReference(databaseRef), updatedData)
             .then(() => {
-                setEditModal(false);
                 updateDate();
+                setEditModal(false);
                 successMessage("Saved the changes.")
             }).catch((error) => {
                 console.error(error.message);
@@ -85,38 +82,18 @@ export default function CardTransaction(props:TransactionInterface) {
     };
 
     return (
-        <div className={"rounded-lg shadow flex w-full p-1 text-start " + bg + " hover:bg-opacity-80 items-center"}>
+        <div className={`${props.amount > 0 ? 'bg-green-900' : 'bg-red-900'} rounded-lg shadow flex w-full p-1 text-start hover:bg-opacity-80 items-center`}>
             <div className="flex-col w-full items-center pl-2 md:pl-6 mr-1 md:pr-2">
                 <div className="w-full mx-auto flex items-center justify-between text-white text-sm md:text-base gap-2 md:gap-6">
                     <div className="flex-wrap w-12 md:w-20">
                         {props.date}
                     </div>
                     <div className="flex-auto">
-                        <div className="font-semibold">{detailsText == "" || detailsText == undefined? props.title : `${props.title} - ${detailsText}`}</div>
+                        <div className="font-semibold">{`${props.title} ${props.details ? `- ${props.details}` : ''}`}</div>
                     </div>
                     <div className="flex-wrap items-center text-base md:text-2xl font-medium sm:mt-0">
                         {formatCurrency(props.amount)}
                     </div>
-                </div>
-                <div className={"border-0 bg-black rounded-md bg-opacity-50"}>
-                    {
-                        data ? (
-                            <UniqueChildren>
-                                {
-                                    data.sort((a, b) => b.key!.localeCompare(a.key!)).map((item) => {
-                                        const snapshot = item.val();
-                                        return (
-                                            <div className="w-full flex justify-between px-2 text-sm pt-1" key={item.key}>
-                                                <div className="flex-wrap w-[6rem]">{(snapshot.details).substring(0, 8)}</div>
-                                                <div className="flex-auto">{(snapshot.details).substring(8,)}</div>
-                                                <div className="flex-wrap">{formatCurrency(snapshot.amount)}</div>
-                                            </div>
-                                        )
-                                    })
-                                }
-                            </UniqueChildren>
-                        ) : null
-                    }
                 </div>
             </div>
             <button className={"mx-2 p-2 bg-black bg-opacity-40 rounded-lg hover:bg-opacity-70 hidden md:block " + (props.access == "admin" ? "" : "hidden")}
@@ -132,31 +109,33 @@ export default function CardTransaction(props:TransactionInterface) {
                 <Modal.Header className="bg-slate-800 rounded-t-md text-white border-t border-x border-blue-500">
                     <div className="text-xl font-medium text-white">Edit Transaction</div>
                 </Modal.Header>
-                <Modal.Body className="bg-slate-950 rounded-b-md border-b border-x border-blue-500">
-                    <form className="space-y-4">
-                        <CustomInput type="text" label="Title"
-                               value={inputTitle}
-                               onChange={(e) => setInputTitle(e.target.value)}
-                               color={inputTitle == "" ? "error" : "default"} helperText={props.title == "" ? "Please input title" : ""}
-                               required
-                        />
-                        <CustomInput type="text" label="Details"
-                               value={inputDetails}
-                               onChange={(e) => setInputDetails(e.target.value)}
-                               color={"default"} helperText={""}
-                        />
-                        <CustomInput label="Amount" value={String(inputAmount)} type="number" pre={`৳ ${inputAmountType == "-" ? "-" : ""}`}
-                               onChange={(e) => setInputAmount(Number(e.target.value))} required/>
-                        <CustomInput label="Date" value={(inputDate)} type="date" disabled
-                               onChange={(e) => setInputDate(e.target.value)}
-                               helperText={"Date cannot be edited"}/>
-
-                        <div className="flex gap-4 justify-center">
-                            <Button color="blue" onClick={handleEdit}>Save</Button>
-                            <Button color="gray" onClick={() => setEditModal(false)}>Cancel</Button>
-                        </div>
-                    </form>
-                </Modal.Body>
+                <Modal.Body className="p-6 bg-slate-950 rounded-b-md border-b border-x border-blue-500">
+                    <CustomInput id={'title'} type="text" label="Title"
+                                 value={transactionData.title}
+                                 onChange={(e) => setTransactionData({...transactionData, title: e.target.value})}
+                                 color={transactionData.title ? "default" : "error"}
+                                 helperText={!props.title ? "Please input title" : ""}
+                                 required
+                    />
+                    <CustomInput id="details" type="text" label="Details"
+                                 value={transactionData.details}
+                                 onChange={(e) => setTransactionData({...transactionData, details: e.target.value})}
+                    />
+                    <CustomInput id="amount" type="number" label="Amount"
+                                 value={transactionData.amount}
+                                 pre={`৳ ${transactionData.type == "-" ? "-" : ""}`}
+                                 onChange={(e) => setTransactionData({...transactionData, amount: Number(e.target.value)})}
+                                 required/>
+                    <CustomDateTimeInput id="date" type="date" label="Date"
+                                         value={transactionData.date}
+                                         onChange={(value) => setTransactionData({...transactionData, date: value})}
+                                         helperText={"Date cannot be edited"} disabled
+                    />
+                    <div className="flex gap-4 justify-center mt-4">
+                        <Button color="blue" onClick={handleEdit}>Save</Button>
+                        <Button color="gray" onClick={() => setEditModal(false)}>Cancel</Button>
+                    </div>
+              </Modal.Body>
             </Modal>
 
             <Modal show={deleteModal} size="md" onClose={() => setDeleteModal(false)} popup className="bg-black bg-opacity-50">
