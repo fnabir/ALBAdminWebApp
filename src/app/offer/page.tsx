01a55f2488d55dbf4d10c62b0a6fb6cc9a8e22df
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-01-07 18:17:27 +0000

    Update Offer page

--- src/app/offer/page.tsx
+++ src/app/offer/page.tsx
@@ -1,157 +1,218 @@
 "use client"
 
-import CardIcon from "@/components/card/CardIcon";
-import Layout from "@/components/Layout";
-import Loading from "@/components/Loading";
-import { useRouter } from "next/navigation";
-import {MdAddCircle, MdDownloading, MdError} from "react-icons/md";
-import {GenerateDatabaseKey, GetDatabaseReference} from "@/firebase/database";
-import CardOffer from "@/components/card/CardOffer";
+import Layout from "@/components/layout"
+import {Button} from "@/components/ui/button";
+import {ScrollArea} from "@/components/ui/scrollArea";
+import {Skeleton} from "@/components/ui/skeleton";
+import CardIcon from "@/components/card/cardIcon";
+import {MdAddCircle, MdError} from "react-icons/md";
+import {DataSnapshot} from "@firebase/database";
+import {
+	Dialog, DialogClose,
+	DialogContent,
+	DialogDescription, DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger
+} from "@/components/ui/dialog";
+import CustomSeparator from "@/components/generic/CustomSeparator";
+import React, {useState} from "react";
 import {useList} from "react-firebase-hooks/database";
-import {Button, Modal} from "flowbite-react";
-import {useState} from "react";
+import {getDatabaseReference} from "@/lib/utils";
+import CardOffer from "@/components/card/cardOffer";
+import {useAuth} from "@/hooks/useAuth";
+import {useForm} from "react-hook-form";
+import {OfferFormData, offerSchema} from "@/lib/schemas";
+import {zodResolver} from "@hookform/resolvers/zod";
 import CustomInput from "@/components/generic/CustomInput";
-import {errorMessage, successMessage} from "@/utils/functions";
 import CustomDropDown from "@/components/generic/CustomDropDown";
-import {useAuth} from "@/context/AuthContext";
-import {update} from "firebase/database";
-import {OfferProductOptions, OfferWorkOptions} from "@/utils/arrays";
-import UniqueChildren from "@/components/UniqueChildrenWrapper";
+import {offerProductOptions, offerWorkOptions} from "@/lib/arrays";
+import {addNewOffer} from "@/lib/functions";
 import {format} from "date-fns";
 
-export default function Offer() {
-  const router = useRouter();
-  const {user, loading} = useAuth();
-  const [ data, dataLoading, dataError ] = useList(GetDatabaseReference('-offer'));
+export default function OfferPage() {
+	const { user, userRole } = useAuth();
+	const [ data, dataLoading, dataError ] = useList(getDatabaseReference('offer'));
+	const [newOfferDialog, setNewOfferDialog] = useState<boolean>(false);
 
-  const [newOfferModal, setNewOfferModal] = useState<boolean>(false);
-  const [offerData, setOfferData] = useState<OfferInterface>({
-    id: "", name: "", product: "", work:""
-  });
+	const breadcrumb: {text: string, link?: string}[] = [
+		{ text: "Home", link: "/" },
+		{ text: "/" },
+		{ text: "Offer" },
+	];
 
-  const handleClick = () => {
-    setOfferData({name: '', product: '', work: '', refer: user.username, uid: user.uid, date: format(new Date(), "dd MMM yyyy")});
-    setNewOfferModal(true);
-  }
+	const {
+		register,
+		reset,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<OfferFormData>({
+		resolver: zodResolver(offerSchema),
+	});
 
-  const saveNewOffer = () => {
-    if (offerData?.name == "") {
-      errorMessage("Project Name required")
-    } else if (offerData?.product == "" || offerData?.product == "Select") {
-      errorMessage("Select product type")
-    } else if (offerData?.work == "" || offerData?.work == "Select") {
-      errorMessage("Select work type")
-    } else {
-      update(GetDatabaseReference(`-offer/${format(new Date(), "yyMMdd")}${GenerateDatabaseKey(`-offer`)}`), offerData)
-        .then(() => {
-          successMessage("Saved the changes.")
-        })
-        .catch((error) => {
-          console.error(error.message);
-          errorMessage(error.message);
-        }).finally(() => {
-          setNewOfferModal(false);
-        })
-    }
-  }
+	const onSubmit = (data: OfferFormData) => {
+		addNewOffer({
+			name: data.name,
+			address: data.address,
+			product: data.product,
+			work: data.work,
+			unit: data.unit,
+			floor: data.floor,
+			person: data.person,
+			shaft: data.shaft,
+			note: data.note,
+			refer: user?.displayName,
+			date: format(new Date(), "dd MMM yyyy"),
+		}).finally(() => {
+			setNewOfferDialog(false);
+			window.location.reload();
+		})
+	}
 
-  if (loading) return <Loading/>
+	const handleReset = () => {
+		reset();
+	};
 
-  if (!loading && !user) return router.push("/login")
-
-  return (
-    <Layout
-      pageTitle="Offer | Asian Lift Bangladesh"
-      headerTitle="Offer">
-      <div>
-        <div className="flex items-center mt-2 gap-x-2">
-          <Button color={"blue"} onClick={handleClick}>
-            <MdAddCircle className="mr-2 h-5 w-5"/>Add New Offer
-          </Button>
-        </div>
-
-        <Modal show={newOfferModal} size="md" popup onClose={() => setNewOfferModal(false)} className="bg-black bg-opacity-50">
-          <Modal.Header className="bg-slate-800 rounded-t-md text-white border-t border-x border-blue-500">
-            <div className="text-xl font-medium text-white">New Offer</div>
-          </Modal.Header>
-          <Modal.Body className="bg-slate-950 rounded-b-md border-b border-x border-blue-500">
-            <div>
-              <CustomInput id={"name"} type="text" label="Project Name"
-                           onChange={(e) => setOfferData({...offerData, name: e.target.value})}
-                           required={true}
-              />
-              <CustomInput id={"address"} type="text" label="Address"
-                           onChange={(e) => setOfferData({...offerData, address: e.target.value})}
-              />
-              <CustomDropDown id="product" label={"Product Type"} options={OfferProductOptions}
-                              onChange={(value) => setOfferData({...offerData, product: value})}
-                              required={true}
-              />
-              <CustomDropDown id="work" label={"Work Type"} options={OfferWorkOptions}
-                              onChange={(value) => setOfferData({...offerData, work: value})}
-                              required={true}
-              />
-              <div className={`flex space-x-1`}>
-                <CustomInput className={`flex-[1_1_50%]`} id={"person"} type="text" label="Person/Load"
-                             onChange={(e) => setOfferData({...offerData, person: e.target.value})}
-                />
-                <CustomInput className={`flex-[1_1_50%]`} id={"floor"} type="text" label="Floor/Stop"
-                             onChange={(e) => setOfferData({...offerData, floor: e.target.value})}
-                />
-              </div>
-              <div className={`flex space-x-1`}>
-                <CustomInput className={`flex-[1_1_30%]`} id={"unit"} type="number" label="Unit" minNumber={1} maxNumber={99}
-                             onChange={(e) => setOfferData({...offerData, unit: e.target.value})}
-                />
-                <CustomInput className={`flex-[1_1_70%]`} id={"shaft"} type="text" label={"Shaft Size (W x D x H)"}
-                             onChange={(e) => setOfferData({...offerData, shaft: e.target.value})}
-                />
-              </div>
-              <CustomInput id={"note"} type="text" label="Remarks"
-                           onChange={(e) => setOfferData({...offerData, note: e.target.value})}
-              />
-              <div className="flex mt-4 gap-4 justify-center">
-                <Button color="blue" onClick={saveNewOffer}>Save</Button>
-                <Button color="gray" onClick={() => setNewOfferModal(false)}>Cancel</Button>
-              </div>
-            </div>
-          </Modal.Body>
-        </Modal>
-
-        <div className="flex flex-col py-2 gap-y-2">
-          {
-            dataLoading ? (
-              <CardIcon title={"Loading"} subtitle={"If data doesn't load in 30 seconds, please refresh the page."}>
-                <MdDownloading className='mx-1 w-6 h-6 content-center'/>
-              </CardIcon>
-            ) : dataError || !data ? (
-              <CardIcon title={"Error"} subtitle={dataError?.message}>
-                <MdError className='mx-1 w-6 h-6 content-center'/>
-              </CardIcon>
-            ) : data.length == 0 ? (
-              <CardIcon title={"No offer Found!"}>
-                <MdError className='mx-1 w-6 h-6 content-center'/>
-              </CardIcon>
-            ) : (
-              <UniqueChildren>
-                {
-                  data.map((item) => {
-                    const snapshot = item.val();
-                    return (
-                      <div className="flex flex-col" key={item.key}>
-                        <CardOffer id={item.key!} name={snapshot.name} address={snapshot.address}
-                                   product={snapshot.product} work={snapshot.work} unit={snapshot.unit}
-                                   floor={snapshot.floor} person={snapshot.person} shaft={snapshot.shaft}
-                                   date={snapshot.date} note={snapshot.note} refer={snapshot.refer}/>
-                      </div>
-                    )
-                  })
-                }
-                </UniqueChildren>
-            )
-          }
-        </div>
-      </div>
-    </Layout>
-  );
+	return (
+		<Layout breadcrumb={breadcrumb}>
+			<div className={"flex flex-col h-full"}>
+				<div className="flex items-center pb-2 gap-x-2">
+					<Dialog open={newOfferDialog} onOpenChange={setNewOfferDialog}>
+						<DialogTrigger asChild>
+							<Button variant={"accent"}>
+								<MdAddCircle/> Add New Offer
+							</Button>
+						</DialogTrigger>
+						<DialogContent className={"border border-blue-500"}>
+							<DialogHeader>
+								<DialogTitle>Edit Offer</DialogTitle>
+								<DialogDescription>
+									Click update to save the changes.
+								</DialogDescription>
+							</DialogHeader>
+							<CustomSeparator orientation={"horizontal"}/>
+							<form onSubmit={handleSubmit(onSubmit)}>
+								<CustomInput id="name"
+														 type="text"
+														 label={"Project Name"}
+														 {...register('name')}
+														 helperText={errors.name ? errors.name.message : ""}
+														 color={errors.name ? "error" : "default"}
+														 required
+								/>
+								<CustomInput id="address"
+														 type="text"
+														 label={"Address"}
+														 {...register('address')}
+								/>
+								<CustomDropDown id="product"
+																label="Product Type"
+																options={offerProductOptions}
+																{...register('product')}
+																helperText={errors.product ? errors.product.message : ""}
+																color={errors.product ? "error" : "default"}
+																required
+								/>
+								<CustomDropDown id="work"
+																label="Work Type"
+																options={offerWorkOptions}
+																{...register('work')}
+																helperText={errors.work ? errors.work.message : ""}
+																color={errors.work ? "error" : "default"}
+																required
+								/>
+								<div className="flex space-x-2">
+									<div className="flex-auto">
+										<CustomInput id="person"
+																 type="text"
+																 label={"Person/Load"}
+																 {...register('person')}
+										/>
+									</div>
+									<div className="flex-auto">
+										<CustomInput id="floor"
+																 type="text"
+																 label={"Floor/Stop"}
+																 {...register('floor')}
+										/>
+									</div>
+								</div>
+								<div className="flex space-x-2">
+									<div className="flex-[1_1_40]">
+										<CustomInput id="unit"
+																 type="text"
+																 label={"Unit"}
+																 {...register('unit')}
+										/>
+									</div>
+									<div className="flex-auto">
+										<CustomInput id="shaft"
+																 type="text"
+																 label={"Shaft Size (W x D x H)"}
+																 {...register('shaft')}
+										/>
+									</div>
+								</div>
+								<CustomInput id="note"
+														 type="text"
+														 label={"Note"}
+														 {...register('note')}
+								/>
+								<DialogFooter className={"sm:justify-center pt-4"}>
+									<DialogClose asChild>
+										<Button type="button" size="lg" variant="secondary">
+											Close
+										</Button>
+									</DialogClose>
+									<Button type="button" size="lg" variant="secondary" onClick={handleReset}>Reset</Button>
+									<Button type="submit" size="lg" variant="accent">Save</Button>
+								</DialogFooter>
+							</form>
+						</DialogContent>
+					</Dialog>
+				</div>
+				<ScrollArea className={"flex-grow -mr-4 pr-4 mb-2"}>
+					{
+						dataLoading ?
+							<div className="p-4 rounded-xl bg-muted/100 flex items-center">
+								<Skeleton className="flex-wrap h-6 w-24 mr-4 rounded-xl"/>
+								<div className={"flex-auto space-y-1"}>
+									<Skeleton className="h-6 w-3/5 rounded-xl"/>
+									<Skeleton className="h-4 w-1/3 rounded-xl"/>
+									<Skeleton className="h-4 w-1/2 rounded-xl"/>
+									<Skeleton className="h-4 w-2/5 rounded-xl"/>
+								</div>
+							</div>
+						: dataError ?
+							<CardIcon
+								title={"Error"}
+								description={dataError.message}>
+								<MdError size={28}/>
+							</CardIcon>
+						: !data || data.length == 0 ?
+							<CardIcon
+								title={"No Record Found"}>
+								<MdError size={28}/>
+							</CardIcon>
+						: <div className={"space-y-2"}>
+							{
+								data.sort((a: DataSnapshot, b: DataSnapshot) => a.key!.localeCompare(b.key!)).map((item: DataSnapshot) => {
+									const snapshot = item.val();
+									return (
+										<div key={item.key}>
+											<CardOffer id={item.key!} name={snapshot.name} address={snapshot.address}
+																 product={snapshot.product} work={snapshot.work} unit={snapshot.unit}
+																 floor={snapshot.floor} person={snapshot.person} shaft={snapshot.shaft}
+																 date={snapshot.date} note={snapshot.note} refer={snapshot.refer} userAccess={userRole}
+											/>
+										</div>
+									)
+								})
+							}
+						</div>
+					}
+				</ScrollArea>
+			</div>
+		</Layout>
+	)
 }
