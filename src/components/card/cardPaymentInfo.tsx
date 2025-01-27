e6112fa71f9d747f06e237b1315883d97f4c6648
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-01-27 10:54:15 +0000

    Add CardPaymentInfo component

---
+++ src/components/card/cardPaymentInfo.tsx
@@ -0,0 +1,64 @@
+import React, {FC, useState} from "react";
+import {MdDelete} from "react-icons/md";
+import {Card} from "@/components/ui/card";
+import {deletePaymentInfo} from "@/lib/functions";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from "@/components/ui/dialog";
+import {Button} from "@/components/ui/button";
+
+const CardPaymentInfo: FC<{type:string, id: string, value: string, userRole: string, className?: string, animationDelay?: number}> = ({
+    type, id, value, userRole, className, animationDelay
+}) => {
+  const [deleteDialog, setDeleteDialog] = useState<boolean>(false);
+
+  const handleDelete = () => {
+    deletePaymentInfo(type, id).finally(() => {
+      setDeleteDialog(false);
+      window.location.reload();
+    });
+  }
+
+  return (
+    <div className="flex space-x-2">
+      <Card className={`flex-auto flex items-center bg-card hover:text-card hover:bg-card-foreground hover:cursor-pointer rounded-lg py-1 px-2 opacity-0 transition-all duration-200 animate-fade-in ${className}`}
+            style={{ animationDelay: `${animationDelay}s` }}>
+        <div className="flex-auto font-mono font-bold">{`${(type === "account" || (type === "cell" && id.length === 8)) ? "***": ""}${id}`}</div>
+        <div>{(type !== "cash") && value}</div>
+      </Card>
+      {
+        userRole === "admin" &&
+        <Dialog open={deleteDialog} onOpenChange={setDeleteDialog}>
+          <DialogTrigger asChild>
+            <Button size="icon"><MdDelete/></Button>
+          </DialogTrigger>
+          <DialogContent className={"border border-destructive"}>
+            <DialogHeader>
+              <DialogTitle>Delete Payment Info</DialogTitle>
+              <DialogDescription>
+                This action cannot be undone. This will permanently delete the payment info.
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter className={"mx-auto"}>
+              <DialogClose asChild>
+                <Button type="button" variant="secondary">
+                  Close
+                </Button>
+              </DialogClose>
+              <Button variant="destructive" onClick={handleDelete}>Delete</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      }
+    </div>
+  )
+}
+
+export default CardPaymentInfo;
