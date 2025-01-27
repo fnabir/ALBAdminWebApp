64e8804297ed290adf076d059dd6329a1382c5b1
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-01-27 10:57:02 +0000

    Add FadeIn animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,15 @@ export default {
   ],
   theme: {
   	extend: {
+			animation: {
+				"fade-in": "fadeIn 0.5s ease-out forwards",
+			},
+			keyframes: {
+				fadeIn: {
+					"0%": { opacity: "0", transform: "translateY(20px)" },
+					"100%": { opacity: "1", transform: "translateY(0)" },
+				},
+			},
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
