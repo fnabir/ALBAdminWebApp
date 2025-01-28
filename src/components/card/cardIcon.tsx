c944d15dc5fae9ad5d1c9524e86b84f7448a6db9
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-01-28 12:39:05 +0000

    Add className option in the CardIcon component

--- src/components/card/cardIcon.tsx
+++ src/components/card/cardIcon.tsx
@@ -2,13 +2,13 @@ import React, { FC } from 'react';
 import Link from "next/link";
 import {Card} from "@/components/ui/card";
 
-const CardIcon: FC<{title: string, number?: number, description?: string, route?: string, children: React.ReactNode}> = ({
-	title, number, description, route, children
+const CardIcon: FC<{title: string, number?: number, description?: string, route?: string, className?: string, children: React.ReactNode}> = ({
+	title, number, description, route, children, className,
 }) => {
 	return(
 		<Link
 			href={route ? route : "#"}>
-			<Card className={"flex w-full px-6 py-4 items-center rounded-xl bg-muted/50 hover:cursor-pointer hover:bg-muted/100 space-x-4"}>
+			<Card className={`flex w-full px-6 py-4 items-center rounded-xl bg-muted/50 hover:cursor-pointer hover:bg-muted/100 space-x-4 ${className}`}>
 				{children}
 				<div className={"flex-1"}>
 					<div className={"text-xl font-bold"}>{title}</div>
