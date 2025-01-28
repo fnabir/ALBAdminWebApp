3d9c23a6e9dd68018f37d1f7d091fab49358d15e
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-01-28 12:38:35 +0000

    Create CardIconVertical Component

---
+++ src/components/card/cardIconVertical.tsx
@@ -0,0 +1,35 @@
+import React, { FC } from 'react';
+import Link from "next/link";
+import {Card} from "@/components/ui/card";
+
+const CardIconVertical: FC<{title: string, number?: number, description?: string, route?: string, className?: string, children: React.ReactNode}> = ({
+	title, number, description, route, children, className,
+}) => {
+	return(
+		<Link
+			href={route ? route : "#"}>
+			<Card className={`flex-col w-full h-full px-6 py-3 items-center text-center rounded-xl bg-muted/50 hover:cursor-pointer hover:bg-muted ${className}`}>
+				<div className={"flex justify-center"}>
+					{
+						number && number > 0 ?
+							<div className={"font-semibold text-primary-foreground text-2xl bg-primary leading-[48px] size-[48px] rounded-full text-center"}>
+								{number}
+							</div>
+						: children
+					}
+
+				</div>
+				<div className={"text-xl font-bold space-x-2 mt-1"}>
+					{title}
+				</div>
+				{description &&
+					<div className={`text-sm text-muted-foreground pb-0`}>
+						{description}
+					</div>
+				}
+			</Card>
+		</Link>
+	);
+};
+
+export default CardIconVertical;
