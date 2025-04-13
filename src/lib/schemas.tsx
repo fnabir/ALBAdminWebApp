4edfe2dab740f7f71a444a52e2fb7fa193acdb9d
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-04-14 00:14:28 +0100

    Fix expense amount validation

--- src/lib/schemas.tsx
+++ src/lib/schemas.tsx
@@ -26,9 +26,6 @@ export const transactionSchema = z.object({
 			message: "Input cannot be empty or not a number",
 		}),
 	date: z.string().nonempty("Date is required"),
-}).refine((data) => data.type === "-" && data.amount !== 0, {
-	message: "Payment amount cannot be 0",
-	path: ["amount"],
 });
 
 export type TransactionFormData = z.infer<typeof transactionSchema>;
