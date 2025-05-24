124b980d1620fe386fd231f8eae216878b574949
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-05-24 21:08:00 +0100

    Update breadcrumbSection.tsx

--- src/components/header/breadcrumbSection.tsx
+++ src/components/header/breadcrumbSection.tsx
@@ -6,28 +6,27 @@ import {
 	BreadcrumbPage,
 	BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
-import {FC} from "react";
-import {breadcrumbItem} from "@/lib/types";
+import React, {FC} from "react";
+import { BreadcrumbInterface } from "@/lib/interfaces";
 
-const BreadcrumbSection: FC<{ breadcrumb: breadcrumbItem[] }> = ({breadcrumb}) => {
+const BreadcrumbSection: FC<{ breadcrumb: BreadcrumbInterface[] }> = ({breadcrumb}) => {
 	return (
 		<Breadcrumb className={"flex-auto"}>
-			<BreadcrumbList>
-				{breadcrumb.map((crumb, index) => (
-					index != breadcrumb.length - 1 ?
-						crumb.text != "/" ?
-							<BreadcrumbItem key={index}>
-								<BreadcrumbLink href={crumb.link}>{crumb.text}</BreadcrumbLink>
-							</BreadcrumbItem>
-							:
-							<BreadcrumbSeparator key={index}/>
-						:
-						<BreadcrumbItem key={index}>
-							<BreadcrumbPage>{crumb.text}</BreadcrumbPage>
-						</BreadcrumbItem>
-				))}
-			</BreadcrumbList>
-		</Breadcrumb>
+      <BreadcrumbList>
+        {breadcrumb.map((crumb, index) => (
+          <React.Fragment key={index}>
+            <BreadcrumbItem>
+              {index === breadcrumb.length - 1 ? (
+                <BreadcrumbPage>{crumb.label}</BreadcrumbPage>
+              ) : (
+                <BreadcrumbLink href={crumb.href}>{crumb.label}</BreadcrumbLink>
+              )}
+            </BreadcrumbItem>
+            {index < breadcrumb.length - 1 && <BreadcrumbSeparator />}
+          </React.Fragment>
+        ))}
+      </BreadcrumbList>
+    </Breadcrumb>
 	)
 }
 
