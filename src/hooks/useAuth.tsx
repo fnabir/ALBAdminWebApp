15cee29ddbe2241e7edfc2e533441dab34ae01ed
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-05-24 21:09:29 +0100

    Update useAuth to read userRole from database

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -2,11 +2,16 @@
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '@/firebase/config';
+import { getDatabaseReference } from '@/lib/utils';
+import { useObject } from 'react-firebase-hooks/database';
 
 export const useAuth = () => {
-	const [user, loading, error] = useAuthState(auth);
-	const admin = ["HrnlOpNxfpTJ4JHjEE7E7lZXJ3n2", "STFXbv1ZzrbxDSWPCMpATVbOekh2", "WhAnZZh7CfNGhe1ejIUlAy1QAh33", "yB7jBuPIo7PxuhpliZ7VNnsL21l2"];
-	const manager = ["LzcKIs2huyaK83FEOqbkJCumezu2"];
-	const userRole = user ? admin.includes(user.uid) ? "admin" : manager.includes(user.uid) ? "manager" : "user" : "";
-	return { user, userRole, loading, error };
+  const [user, userLoading, userError] = useAuthState(auth);
+
+  const userRef = user ? getDatabaseReference(`info/user/${user.uid}`) : null;
+  const userData = useObject(userRef)[0]?.val();
+
+  const userRole = userData?.role ?? "";
+  const isAdmin = userRole  === "admin";
+	return { user, isAdmin, userLoading, userError };
 };
