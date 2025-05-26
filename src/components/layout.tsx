ae7da13bb6b68fba23ad80e0d7a7d056f71e3842
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-05-26 10:40:13 +0100

    Update layout.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,19 +5,35 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
-import { FC, ReactNode } from "react";
+import { FC, ReactNode, useEffect, useState } from "react";
 import {ThemeToggle} from "@/components/header/themeToggle";
 import { BreadcrumbInterface } from "@/lib/interfaces";
-import BreadcrumbSection from "./header/breadcrumbSection";
+import BreadcrumbSection from "@/components/header/breadcrumbSection";
+import Loading from "@/components/loading";
 
-const Layout: FC<{ breadcrumb: BreadcrumbInterface[], children: ReactNode }> = ({
-    breadcrumb, children
-  }) => {
-    return(
+const Layout: FC<{ breadcrumb: BreadcrumbInterface[], children: ReactNode }> = ({breadcrumb, children}) => {
+  const [showLoader, setShowLoader] = useState(true);
+  
+  useEffect(() => {
+    const handleLoad = () => {
+      setTimeout(() => setShowLoader(false), 600);
+    };
+
+    if (document.readyState === "complete") {
+      handleLoad();
+    } else {
+      window.addEventListener("load", handleLoad);
+      return () => window.removeEventListener("load", handleLoad);
+    }
+  }, []);
+
+  return(
+    <>
+      {showLoader && <Loading />}
       <SidebarProvider>
         <AppSidebar />
         <SidebarInset>
-          <div className={"flex flex-col h-screen"}>
+          <div className={"flex flex-col h-screen pb-2"}>
             <header
               className="flex px-4 py-2 h-14 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
               <SidebarTrigger className="-ml-1"/>
@@ -25,41 +41,41 @@ const Layout: FC<{ breadcrumb: BreadcrumbInterface[], children: ReactNode }> = (
               <BreadcrumbSection breadcrumb={breadcrumb}/>
               <ThemeToggle/>
             </header>
-            <div className="flex-1 gap-4 px-4 overflow-y-auto">
+            <div className="flex-1 px-2 md:px-4 overflow-y-auto">
               {children}
             </div>
-            <footer
-              className={`flex text-center shrink-0 items-center md:justify-between gap-2 mx-4 my-2 transition-[width,height] ease-linear p-4 rounded-xl bg-muted/100 text-sm text-primary`}>
-              <span className="flex-1 text-center md:text-left">
-                © 2024 <a href="https://asianliftbd.com/" className="hover:underline">Asian Lift Bangladesh</a>. All Rights Reserved.
+            <footer className="hidden md:flex shrink-0 items-center md:justify-between space-x-4 mx-4 my-2 p-4 rounded-xl bg-muted text-sm">
+              <span>
+                © {new Date().getFullYear()} <a href="https://asianliftbd.com/" className="hover:underline">Asian Lift Bangladesh</a>. All Rights Reserved.
               </span>
-                <ul className="flex space-x-4">
-                  <li>
-                    <a
-                      className="hover:underline"
-                      href="https://asianliftbd.com/privacy-policy"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Privacy Policy
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="hover:underline"
-                      href="https://asianliftbd.com/terms-of-use"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Terms Of Use
-                    </a>
-                  </li>
-                </ul>
+              <ul className="flex space-x-4">
+                <li>
+                  <a
+                    className="hover:underline"
+                    href="https://asianliftbd.com/privacy-policy"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Privacy Policy
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="hover:underline"
+                    href="https://asianliftbd.com/terms-of-use"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Terms Of Use
+                  </a>
+                </li>
+              </ul>
             </footer>
           </div>
         </SidebarInset>
       </SidebarProvider>
-    )
+    </>
+  )
 }
 
 export default Layout;
