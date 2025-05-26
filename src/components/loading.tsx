58e0411db3ad6cdc1d60ec79446f2b78eba12172
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-05-26 10:37:59 +0100

    Update loading.tsx

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,11 +1,11 @@
 export default function Loading() {
 	return (
-		<div className='w-full h-screen flex items-center flex-col justify-center'>
+		<div className="absolute w-full h-screen flex items-center flex-col justify-center">
 			<span className="relative flex h-10 w-10 justify-center items-center m-8">
 				<span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-500 opacity-75"></span>
 				<span className="relative inline-flex rounded-full h-5 w-5 bg-sky-500"></span>
 			</span>
-			<h2 className='text-2xl font-bold'>Loading, please wait...</h2>
+			<h2 className="text-2xl font-bold">Loading, please wait...</h2>
 		</div>
 	);
 };
