4a420c773172837c2346d7247eee6d30661d5900
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-05-29 18:42:09 +0100

    Update cardBalance.tsx

--- src/components/card/cardBalance.tsx
+++ src/components/card/cardBalance.tsx
@@ -1,33 +1,39 @@
 import { formatCurrency } from "@/lib/utils";
-import Link from "next/link";
+import Link from "@/components/link";
 import {Badge} from "@/components/ui/badge";
 import {Card, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
 import {BalanceInterface} from "@/lib/interfaces";
 
-export default function CardBalance(props:BalanceInterface) {
+interface CardBalanceProps extends BalanceInterface {
+  animationDelay?: number;
+}
+
+export default function CardBalance({
+  id, name, value, date, type, status, animationDelay = 0}: CardBalanceProps) {
+    const getCardStyle = () => {
+      if (status === "cancel") return "bg-red-900 text-white hover:bg-red-900/80";
+      if (value < 0) return "bg-yellow-900 text-white hover:bg-yellow-900/80";
+      if (value === 0) return "bg-green-900 text-white hover:bg-green-900/80";
+      return "bg-muted text-primary hover:bg-muted/80";
+    };
+
+    const badgeLabel = status === "cancel" ? "Cancelled" : value < 0 ? "Overpaid" : null;
+    
     return (
-      <Link
-        href={`/${props.type}/${props.id}`}>
-          <Card
-            className={`flex w-full justify-between items-center px-6 py-2 hover:bg-opacity-75
-                        ${props.status === "cancel" ? "bg-red-900 text-white" : 
-                          props.value < 0 ? "bg-yellow-900 text-white" : 
-                          props.value === 0 ? "bg-green-900 text-white" : "bg-muted hover:bg-muted/80 text-primary" }`}
-            >
-            <div className={"items-center"}>
-              <CardHeader className={"flex-row items-center space-x-2 space-y-0 p-0"}>
-                <CardTitle className="font-semibold">{props.name}</CardTitle>
-                {
-                  (props.status === "cancel" || props.value < 0) &&
-                  <Badge>{props.status == 'cancel' ? "Cancelled" : props.value < 0 ? "Overpaid" : ""}</Badge>
-                }
-              </CardHeader>
-              { props.date && <CardFooter className="text-sm p-0">{props.date}</CardFooter>}
-            </div>
-            <div className={"flex items-center text-2xl font-medium font-mono"}>
-                {formatCurrency(props.value)}
-            </div>
-          </Card>
-        </Link>
+      <Link href={`/${type}/${id}`} className={animationDelay !== 0 ? "opacity-0 animate-fade-in-x" : ""}
+						style={{animationDelay: `${animationDelay}s`}}>
+        <Card className={`flex w-full justify-between items-center px-2 lg:px-6 py-2 ${getCardStyle()} transition-all duration-150`}>
+          <div className={"flex-grow items-center"}>
+            <CardHeader className={"flex-row items-center space-x-2 space-y-0 p-0"}>
+              <CardTitle className="font-semibold">{name}</CardTitle>
+              {badgeLabel && <Badge>{badgeLabel}</Badge>}
+            </CardHeader>
+            { date && <CardFooter className="text-sm p-0">{date}</CardFooter>}
+          </div>
+          <div className={"flex-wrap items-center text-2xl font-medium font-mono"}>
+              {formatCurrency(value)}
+          </div>
+        </Card>
+      </Link>
     )
 }
