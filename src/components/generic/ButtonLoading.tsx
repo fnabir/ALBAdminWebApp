920836a59211cfd9544aeb0717d6b3ede5123dda
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-06-02 19:17:41 +0100

    Add button variant option

--- src/components/generic/ButtonLoading.tsx
+++ src/components/generic/ButtonLoading.tsx
@@ -5,12 +5,13 @@ type ButtonLoadingProps = {
 	loading: boolean;
   text: string;
   loadingText: string;
+  variant?: "default" | "destructive" | "outline" | "secondary" | "accent";
   className?: string;
 };
 
-export function ButtonLoading({type, loading, text, loadingText, className} : ButtonLoadingProps) {
+export function ButtonLoading({type, loading, text, loadingText, variant, className} : ButtonLoadingProps) {
   return (
-    <Button type={type} className={`transition-all duration-150 ${className}`} disabled={loading }>
+    <Button type={type} variant={variant ?? "default"} className={`transition-all duration-150 ${className}`} disabled={loading}>
       {loading && (
         <div className="size-4 animate-spin rounded-full border-2 border-primary-foreground border-t-transparent" />
       )}
