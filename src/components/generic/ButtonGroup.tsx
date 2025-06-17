d848a7a6eda9ccf57e664e650f52f33c7ebaf45a
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-06-17 17:07:05 +0100

    Create ButtonGroup component

---
+++ src/components/generic/ButtonGroup.tsx
@@ -0,0 +1,27 @@
+import { OptionsInterface } from "@/lib/interfaces";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+type Props = {
+  title?: string
+  options: {value:string, label?: string, disabled?: boolean}[];
+  value: string;
+  onChange: (value: string) => void;
+  className?: string;
+};
+
+export function ButtonGroup({ title, options, value, onChange, className }: Props) {
+  return (
+    <div className={`md:inline-flex space-x-2 items-center pr-2 ${className}`}>
+      {title && <div className="text-primary text-sm">{title}</div>}
+      <Tabs value={value} onValueChange={onChange}>
+        <TabsList className="inline-flex rounded-md bg-muted p-1 shadow-sm">
+          {options.map((option) => (
+            <TabsTrigger key={option.value} value={option.value} disabled={option.disabled ?? false}>
+              {option.label ?? option.value}
+            </TabsTrigger>
+          ))}
+        </TabsList>
+      </Tabs>
+    </div>
+  )
+}
