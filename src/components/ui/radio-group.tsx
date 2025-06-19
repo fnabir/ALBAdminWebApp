950e7250c42742214a96a389ec46be98a5aebecc
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-06-19 16:39:19 +0100

    Style adjustment

--- src/components/ui/radio-group.tsx
+++ src/components/ui/radio-group.tsx
@@ -34,7 +34,7 @@ const RadioGroupItem = React.forwardRef<
       {...props}
     >
       <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
-        <FaCircle className="h-3.5 w-3.5 fill-primary" />
+        <FaCircle className="h-2 w-2 fill-primary" />
       </RadioGroupPrimitive.Indicator>
     </RadioGroupPrimitive.Item>
   )
